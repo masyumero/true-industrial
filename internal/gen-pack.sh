5a7387ae8e82d53d75c0be480e1e3434c0349175
masyumero <167562220+masyumero@users.noreply.github.com>
2025-04-18 00:05:39 +0900

    gen-pack.sh Fix

--- internal/gen-pack.sh
+++ internal/gen-pack.sh
@@ -27,7 +27,7 @@ cp manifest.json packtmp/
 
 echoerr packtmp/overridesディレクトリを構築
 mkdir packtmp/overrides/
-cp -r config/ kubejs/ ldlib/ mods/ defaultconfigs/ packtmp/overrides/
+cp -r config/ kubejs/ ldlib/ defaultconfigs/ packtmp/overrides/
 
 echoerr 既存のpackディレクトリを削除
 rm -rf pack/ || true
